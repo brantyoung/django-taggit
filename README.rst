a898b09b41842893e175d6464f612f865e0613df
Brant Young <brant.young@gmail.com>
2012-08-20 22:27:14 +0800

    Add support for Chinese comma, and do not treat space as tag string delimiter

---
+++ README.rst
@@ -0,0 +1,56 @@
+django-taggit
+=============
+
+和最初 `alex/django-taggit <https://github.com/alex/django-taggit>`_ 的区别
+--------------------------------------------------------------------------------
+
+#. 增加 ``TAGGIT_FORCE_LOWERCASE = True`` 配置项，启用后所有标签都会转换成小写对待；
+#. 只认中、英文逗号（``，`` 和 ``,``）作为标签的分割符，空格在任何情况下都被认为是标签的一部分。
+
+一般使用方法
+--------------
+
+``django-taggit`` 是一个简单易用的 Django 标签 app。把 ``"taggit"`` 加到在你项目中的 ``INSTALLED_APPS`` 中，
+然后为你的 model 增加 TaggableManager 就完工了 ::
+
+    from django.db import models
+
+    from taggit.managers import TaggableManager
+
+    class Food(models.Model):
+        # ... fields here
+
+        tags = TaggableManager()
+
+接下来你便可以按如下方式使用 API::
+
+    >>> apple = Food.objects.create(name="apple")
+    >>> apple.tags.add("red", "green", "delicious")
+    >>> apple.tags.all()
+    [<Tag: red>, <Tag: green>, <Tag: delicious>]
+    >>> apple.tags.remove("green")
+    >>> apple.tags.all()
+    [<Tag: red>, <Tag: delicious>]
+    >>> Food.objects.filter(tags__name__in=["red"])
+    [<Food: apple>, <Food: cherry>]
+
+Tags 将会自动显示在表单和后台 admin 中。
+
+
+
+If you want to enforce lowercase tags everywhere (recommended, to avoid
+ending up with tags 'Music' and 'music' which are functionally identical
+but show up in different taxonomies), add to settings.py:
+
+``TAGGIT_FORCE_LOWERCASE = True``
+
+If you want to prevent certain words from being added as tags (such as
+English articles to, from, the, of, etc.) add to settings.py:
+
+``TAGGIT_STOPWORDS = [u'a', u'an', u'and', u'be', u'from', u'of']``
+
+``django-taggit`` requires Django 1.1 or greater.
+
+For more info checkout out the documentation.  And for questions about usage or
+development you can contact the
+`mailinglist <http://groups.google.com/group/django-taggit>`_.
--- taggit/utils.py
+++ taggit/utils.py
@@ -1,18 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from django.utils.encoding import force_unicode
 from django.utils.functional import wraps
 from django.template.defaultfilters import lower
 from django.conf import settings
-
-def stopwords(words):
-    # Shacker fork - remove any stopwords defined in settings
-    try:
-        stoplist = settings.TAGGIT_STOPWORDS
-        words = list(set(words) - set(stoplist))
-    except:
-        pass
-
-    return words
-
 
 def parse_tags(tagstring):
     """
@@ -35,68 +26,8 @@ def parse_tags(tagstring):
     except:
         tagstring = force_unicode(tagstring)
 
-    # Special case - if there are no commas or double quotes in the
-    # input, we don't *do* a recall... I mean, we know we only need to
-    # split on spaces.
-    if u',' not in tagstring and u'"' not in tagstring:
-        words = list(set(split_strip(tagstring, u' ')))
-        words.sort()
-
-        # shacker fork - remove any defined stopwords
-        words = stopwords(words)
-
-        return words
-
-    words = []
-    buffer = []
-    # Defer splitting of non-quoted sections until we know if there are
-    # any unquoted commas.
-    to_be_split = []
-    saw_loose_comma = False
-    open_quote = False
-    i = iter(tagstring)
-    try:
-        while True:
-            c = i.next()
-            if c == u'"':
-                if buffer:
-                    to_be_split.append(u''.join(buffer))
-                    buffer = []
-                # Find the matching quote
-                open_quote = True
-                c = i.next()
-                while c != u'"':
-                    buffer.append(c)
-                    c = i.next()
-                if buffer:
-                    word = u''.join(buffer).strip()
-                    if word:
-                        words.append(word)
-                    buffer = []
-                open_quote = False
-            else:
-                if not saw_loose_comma and c == u',':
-                    saw_loose_comma = True
-                buffer.append(c)
-    except StopIteration:
-        # If we were parsing an open quote which was never closed treat
-        # the buffer as unquoted.
-        if buffer:
-            if open_quote and u',' in buffer:
-                saw_loose_comma = True
-            to_be_split.append(u''.join(buffer))
-    if to_be_split:
-        if saw_loose_comma:
-            delimiter = u','
-        else:
-            delimiter = u' '
-        for chunk in to_be_split:
-            words.extend(split_strip(chunk, delimiter))
-    words = list(set(words))
-    words.sort()
-
-    # shacker fork - remove any defined stopwords
-    words = stopwords(words)
+    tagstring = tagstring.replace(u'，', ', ')
+    words = split_strip(tagstring)
 
     return words
 
@@ -117,29 +48,8 @@ def split_strip(string, delimiter=u','):
 
 
 def edit_string_for_tags(tags):
-    """
-    Given list of ``Tag`` instances, creates a string representation of
-    the list suitable for editing by the user, such that submitting the
-    given string representation back without changing it will give the
-    same list of tags.
-
-    Tag names which contain commas will be double quoted.
-
-    If any tag name which isn't being quoted contains whitespace, the
-    resulting string of tag names will be comma-delimited, otherwise
-    it will be space-delimited.
-
-    Ported from Jonathan Buchanan's `django-tagging
-    <http://django-tagging.googlecode.com/>`_
-    """
-    names = []
-    for tag in tags:
-        name = tag.name
-        if u',' in name or u' ' in name:
-            names.append('"%s"' % name)
-        else:
-            names.append(name)
-    return u', '.join(sorted(names))
+    names = [tag.name for tag in tags]
+    return u', '.join(names)
 
 
 def require_instance_manager(func):
